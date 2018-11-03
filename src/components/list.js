22c4c74fc7df4e97d7e255f7a3d409c2863f8732
chriskim2311 <chriskim2311@gmail.com>
2018-11-02 17:56:38 -0700

    Initial project set up complete with dummy data

---
+++ src/components/list.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import listData from '../dummy_data/list';
+
+class List extends Component{
+    constructor(props) {
+        super(props);
+        this.state={
+            list: []
+        }
+    }
+
+    componentDidMount() {
+        this.getListData();
+    }
+
+    getListData(){
+        this.setState({
+            list: listData
+        })
+    }
+
+
+
+
+    render(){
+        console.log('State:', this.state)
+
+        const listElements = this.state.list.map((item,index)=>{
+            return <li className='collection-item' key= {item._id}>{item.title}</li>;
+        })
+        return(
+            <ul className='collection'>
+                {listElements}
+            </ul>
+        )
+    }
+}
+
+
+
+export default List;
---
+++ src/dummy_data/list.js
@@ -0,0 +1,22 @@
+export default[
+    {
+        title: 'Go to store',
+        details: 'Get eggs, steak, milk and diet coke',
+        _id:  '001'
+    },
+    {
+        title: 'Clean the house',
+        details: 'vacuum and mop the floors',
+        _id: '002'
+    },
+    {
+        title: 'Get exercise',
+        details: 'play basketball',
+        _id: '003'
+    },
+    {
+        title: 'Go to work',
+        details: 'work events',
+        _id: '004'
+    }
+]
